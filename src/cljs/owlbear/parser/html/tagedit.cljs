b25b6b0e64c8e9be1f88c99e12ba90a02dc4a85b
sepehr ansaripour <pehrans@gmail.com>
2022-01-01 12:30:26 -0500

    :recycle: Consolidate tagedit functions in tagedit ns

---
+++ src/cljs/owlbear/parser/html/tagedit.cljs
@@ -0,0 +1,6 @@
+(ns cljs.owlbear.parser.html.tagedit
+  (:require [owlbear.parser.html.tagedit.barf :as obp-html-barf]
+            [owlbear.parser.html.tagedit.slurp :as obp-html-slurp]))
+
+(def forward-barf obp-html-barf/forward-barf)
+(def forward-slurp obp-html-slurp/forward-slurp)
