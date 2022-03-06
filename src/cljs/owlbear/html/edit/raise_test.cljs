d05510aa6a45f1d8d9f86e82b9f770b76a89494f
sepehr ansaripour <pehrans@gmail.com>
2022-03-06 17:34:00 -0500

    #18 :white_check_mark: Add raise test

--- src/cljs/owlbear/html/edit/raise_test.cljs
+++ src/cljs/owlbear/html/edit/raise_test.cljs
@@ -1,8 +1,65 @@
 (ns owlbear.html.edit.raise-test
   (:require [cljs.test :refer [deftest is testing]]
-            [owlbear.utilities :as obu]
-            [owlbear.html.edit.raise :as ob-html-raise]))
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [owlbear.generators.tree.html :as obgt-html]
+            [owlbear.html.edit.raise :as ob-html-raise]
+            [owlbear.html.parse :as ob-html]
+            [owlbear.html.parse.rules :as ob-html-rules]
+            [owlbear.parse.rules :as obpr]
+            [owlbear.utilities :refer [noget+]]))
+
+(defspec raise-spec 5
+  (prop/for-all [{:keys [src
+                         in-bounds-offset
+                         out-of-bounds-offset
+                         parent-node-start-offset
+                         current-node-text
+                         current-node-start-index
+                         current-node-end-index]} (gen/let [tree (obgt-html/tree {:hiccup-gen-opts
+                                                                                  {:vector-gen-args [1 6]
+                                                                                   :tag-name-gen obgt-html/html-element-container-tag-name}})]
+                                                    (let [root-node (noget+ tree :?rootNode)
+                                                          raise-subjects (filter (comp #(noget+ % :?parent.?parent) ob-html-rules/object-node)
+                                                                                 (obpr/flatten-children root-node))]
+                                                      (gen/let [current-node (gen/elements raise-subjects)
+                                                                in-bounds-offset (gen/elements [(noget+ current-node :?startIndex) (dec (noget+ current-node :?endIndex))])]
+                                                        {:src (noget+ root-node :?text)
+                                                         :parent-node-text (noget+ current-node :?parent.?text)
+                                                         :parent-node-start-offset (noget+ current-node :?parent.?startIndex)
+                                                         :in-bounds-offset in-bounds-offset
+                                                         :out-of-bounds-offset (inc (noget+ root-node :?endIndex))
+                                                         :current-node-text (noget+ current-node :?text)
+                                                         :current-node-start-index (noget+ current-node :?startIndex)
+                                                         :current-node-end-index (noget+ current-node :?endIndex)})))]
+    (testing "when cursor out of bounds"
+      (is (nil? (ob-html-raise/raise src out-of-bounds-offset))
+          "no result"))
+    (testing "when cursor in bounds"
+      (let [result (ob-html-raise/raise src in-bounds-offset)
+            result-offset (noget+ result :?offset)
+            result-src (noget+ result :?src)]
+        (is (some? result)
+            "raise performed")
+        (is (string? result-src)
+            "resulting text is a string")
+        (is (number? result-offset)
+            "resulting offset is a number")
+        (is (< (count result-src) (count src))
+            "resulting text length is shorter")
+        (is (< result-offset in-bounds-offset)
+            "resulting offset is smaller")
+        (is (= (-> result-src
+                   ob-html/src->tree
+                   (noget+ :?rootNode)
+                   (ob-html-rules/node->current-object-nodes parent-node-start-offset)
+                   last
+                   (noget+ :?text))
+               current-node-text)
+            "parent start offset now points to current node")))))
 
 (deftest raise-test
-  (let [test-result (ob-html-raise/raise "<div><h1></h1></div>" 7)]
-    (is (= "<h1></h1>" (obu/noget+ test-result :src)))))
+  (testing "when src is empty"
+    (is (nil? (ob-html-raise/raise "" 0))
+        "no result")))
