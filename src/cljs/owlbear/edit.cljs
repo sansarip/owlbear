20b7b37bc9e4e9cd2f1ebcf81208ac2842a7837f
Sepehr Ansaripour <pehrans@gmail.com>
2022-05-23 00:18:41 -0400

    #40 :sparkles: Add TS/X forward barf commands

--- src/cljs/owlbear/edit.cljs
+++ src/cljs/owlbear/edit.cljs
@@ -5,11 +5,14 @@
             [owlbear.html.edit.kill :as obp-kill]
             [owlbear.html.edit.raise :as ob-html-raise]
             [owlbear.html.edit.slurp :as obp-slurp]
+            [owlbear.ts.edit.barf :as ob-ts-barf]
             [owlbear.ts.edit.slurp :as ob-ts-slurp]))
 
 (def html-forward-barf (comp ->js obp-barf/forward-barf))
 (def html-forward-slurp (comp ->js obp-slurp/forward-slurp))
 (def html-kill (comp ->js obp-kill/kill))
 (def html-raise (comp ->js ob-html-raise/raise))
+(def ts-forward-barf (comp ->js ob-ts-barf/forward-barf))
+(def tsx-forward-barf (comp ->js #(ob-ts-barf/forward-barf % %2 :tsx)))
 (def ts-forward-slurp (comp ->js ob-ts-slurp/forward-slurp))
 (def tsx-forward-slurp (comp ->js #(ob-ts-slurp/forward-slurp % %2 :tsx)))
