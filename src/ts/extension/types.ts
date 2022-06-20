b34c7005d4bb2eae15544c4c544036e157c74069
Sepehr Ansaripour <pehrans@gmail.com>
2022-06-20 12:36:05 -0400

    #64 :sparkles: Implement copy

--- src/ts/extension/types.ts
+++ src/ts/extension/types.ts
@@ -1,1 +1,3 @@
 export type EditCtx = { src: string; offset: number; removedText: string };
+export type DocCtx = Pick<EditCtx, "src" | "offset">;
+export type OwlbearFunction = (src: string, offset: number) => EditCtx;
--- src/ts/extension/utilities.ts
+++ src/ts/extension/utilities.ts
@@ -7,7 +7,7 @@ import {
   TextEditor,
   TextEditorEdit,
 } from "vscode";
-import { EditCtx } from "./types";
+import { DocCtx, EditCtx, OwlbearFunction } from "./types";
 
 export const textRange = (document: TextDocument): Range => {
   const firstLine = document.lineAt(0);
@@ -15,19 +15,25 @@ export const textRange = (document: TextDocument): Range => {
   return new Range(firstLine.range.start, lastLine.range.end);
 };
 
+export const getCurrentOffset = (editor: TextEditor): number => {
+  return editor.document.offsetAt(editor.selection.active);
+}
+
 export const replace = async (
   editor: TextEditor,
-  cursorOffset: number,
   editCtx: EditCtx
 ): Promise<EditCtx | undefined> => {
   const { src: newSrc, offset: newCursorOffset } = editCtx;
   const didEdit = await editor.edit((editBuilder: TextEditorEdit) => {
+    // Replace the entire document with the new text
     if (!newSrc) {
       return false;
     }
     editBuilder.replace(textRange(editor.document), newSrc);
 
-    if (isNaN(newCursorOffset) || newCursorOffset === cursorOffset) {
+    // Move cursor to the new offset
+    const currentOffset = getCurrentOffset(editor);
+    if (isNaN(newCursorOffset) || newCursorOffset === currentOffset) {
       return false;
     }
     const newCursorPosition: Position =
@@ -50,3 +56,18 @@ export const replace = async (
 
 export const getFileExtension = (document: TextDocument | undefined) =>
   document?.uri.toString().split(".")?.pop()?.toLowerCase();
+
+export const getDocCtx = (editor: TextEditor): DocCtx => {
+  const document = editor.document;
+  const cursorOffset = document.offsetAt(editor.selection.active);
+  const source = document.getText();
+  return { src: source, offset: cursorOffset };
+};
+
+export const getEditCtx = (
+  editor: TextEditor,
+  obFn: OwlbearFunction
+): EditCtx => {
+  const { src, offset } = getDocCtx(editor);
+  return obFn(src, offset);
+};
